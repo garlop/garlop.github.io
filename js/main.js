e1f2d7e421f78d281d76e7c177df47c59135c89c
Luis Alberto Garnica López <lagl95@hotmail.com>
2019-07-11 18:07:14 -0500

    changing social networks links 2

--- js/main.js
+++ js/main.js
@@ -3,13 +3,13 @@ function changeResume(parameter){
         var new_url = "files/cvEsp.pdf";
             $('#resumeShow').attr('data', new_url);
             $('#resumeAlt').attr('href', new_url);
-            $('#resumeShow').load(new_url);
+            //$('#resumeShow').load(new_url);
             //$('#resumeAlt').load(new_url);
     }else{
         var new_url = "files/cvEng.pdf";
             $('#resumeShow').attr('data', new_url);
             $('#resumeAlt').attr('href', new_url);
-            $('#resumeShow').load(new_url);
+            //$('#resumeShow').load(new_url);
             //$('#resumeAlt').load(new_url);
     }
 }
