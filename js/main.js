7f48abc88272f98e4db3d8f39bfebe9ed842cca4
Luis Alberto Garnica López <lagl95@hotmail.com>
2019-07-11 17:58:49 -0500

    commenting things just to try

--- js/main.js
+++ js/main.js
@@ -4,12 +4,12 @@ function changeResume(parameter){
             $('#resumeShow').attr('data', new_url);
             $('#resumeAlt').attr('href', new_url);
             $('#resumeShow').load(new_url);
-            $('#resumeAlt').load(new_url);
+            //$('#resumeAlt').load(new_url);
     }else{
         var new_url = "files/cvEng.pdf";
             $('#resumeShow').attr('data', new_url);
             $('#resumeAlt').attr('href', new_url);
             $('#resumeShow').load(new_url);
-            $('#resumeAlt').load(new_url);
+            //$('#resumeAlt').load(new_url);
     }
 }
